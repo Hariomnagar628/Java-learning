06e053b704d1146120a972fe78dc795bb1d7712a
Hariom <hariomnagar628@gmail.com>
2025-02-17 00:31:25 +0530

    Ends with method

---
+++ Stringmethods/Endswith.java
@@ -0,0 +1,10 @@
+package Stringmethods;
+
+public class Endswith {
+    public static void main(String[] args) {
+        String name = "Hariomnagar";
+
+        Boolean value = name.endsWith("nagar");
+        System.out.println(value);
+    }
+}
