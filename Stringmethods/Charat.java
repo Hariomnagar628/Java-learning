714042bd0eb305b2652a5b3ffb87d7bf8f5dcea5
Hariom <hariomnagar628@gmail.com>
2025-02-17 10:08:16 +0530

    Charat method added

---
+++ Stringmethods/Charat.java
@@ -0,0 +1,12 @@
+package Stringmethods;
+
+public class Charat {
+    public static void main(String[] args) {
+        String name = "HariomNagar";
+
+         char x = name.charAt(2);
+         System.out.println(x);
+
+
+    }
+}
