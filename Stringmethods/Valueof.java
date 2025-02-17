fe17ac51f239725b8d2232ad73323a7bd7388038
Hariom <hariomnagar628@gmail.com>
2025-02-17 10:19:46 +0530

    Valueof function

---
+++ Stringmethods/Valueof.java
@@ -0,0 +1,16 @@
+package Stringmethods;
+
+public class Valueof {
+    
+    public static void main(String[] args) {
+        
+        // To convert any number into string 
+        int num = 123;
+
+        String number = String.valueOf(num);
+        System.out.println(number+2);
+        System.out.println(num+2);
+        
+
+    }
+}
