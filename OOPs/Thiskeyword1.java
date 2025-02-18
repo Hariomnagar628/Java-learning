df597817877cc0ae7382870e7e99d227d574e88f
Hariom <hariomnagar628@gmail.com>
2025-02-19 00:15:34 +0530

    this keywords files added

---
+++ OOPs/Thiskeyword1.java
@@ -0,0 +1,58 @@
+package OOPs;
+
+// 1. Referring to Instance Variables
+// this keyword is used to refer to the current object inside a method or a   constructor.
+
+
+
+
+//  Referring to Instance Variables
+//  When a method parameter has the same name as an instance variable, this is used to          differentiate between them.
+
+// The constructor parameters (name and age) have the same names as the instance variables.
+// this.name refers to the instance variable, while name refers to the parameter.
+
+
+// If a parameter name matches an instance variable name, Java assumes you are referring to the parameter, not the instance variable.
+// this is necessary to explicitly refer to the instance variable when there's a naming conflict.
+
+
+class Student{
+
+    String name;
+    int age;
+
+     Student(String name,int age)
+    {
+
+        this.name = name;
+        this.age = age;
+
+
+    } 
+
+    void Display()
+    {
+        System.out.println("Name:"+name+", Age:"+age);
+    }
+
+}
+
+
+public class Thiskeyword1 {
+   
+    public static void main(String[] args) {
+
+
+         Student s1 = new Student("Hariom", 23);
+         s1.Display();
+
+        
+    }
+
+}
+
+
+
+
+
---
+++ OOPs/Thiskeyword2.java
@@ -0,0 +1,53 @@
+package OOPs;
+
+// 2. Calling Another Constructor (Constructor Chaining)
+
+// The this keyword can be used to call another constructor in the same class.
+
+
+class Student1{
+
+     String name;
+     int age;
+
+    Student1(String name)
+    {
+         
+        this(name,23);
+
+    }
+
+    Student1(String name,int age)
+    {
+
+          this.name = name;
+          this.age = age;
+
+    }
+
+    void Display()
+    {
+        System.out.println(name+","+age);
+    }
+}
+
+
+
+
+
+public class Thiskeyword2 {
+    
+
+     public static void main(String [] args)
+     {
+
+        Student1 s1 = new Student1("Hariom");
+        s1.Display();
+
+     }
+}
+
+
+
+
+
