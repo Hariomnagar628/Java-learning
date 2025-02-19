ae57f886e41099776d4a074e5e5bb9fbbd89cb5a
Hariom <hariomnagar628@gmail.com>
2025-02-19 23:03:14 +0530

    changes

--- OOPs1/OOPs2/inheritance.java
+++
@@ -1,8 +0,0 @@
-package OOPs1.OOPs2;
-
-public class inheritance {
-    
-        public static void main(String[] args) {
-           
-        }
-}
---
+++ OOPs2/Inheritance.java
@@ -0,0 +1,10 @@
+package OOPs2;
+
+public class Inheritance {
+ 
+    public static void main(String[] args) {
+        
+    }
+
+}
+
